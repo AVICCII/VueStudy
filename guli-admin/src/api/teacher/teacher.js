b21655c198a140ccc37a463220eb6b0384761b98
AVICCII <863134471@qq.com>
2021-03-03 18:59:56 +0800

    2020/03/03

---
+++ guli-admin/src/api/teacher/teacher.js
@@ -0,0 +1,16 @@
+import request from '@/utils/request'
+
+export default{
+
+    //1.讲师列表
+    //current当前页，limit每页记录数，teacherQuery条件对象
+    getTeacherListPage(current,limit,teacherQuery){
+        return request({
+            url: `/eduService/edu-teacher/pageTeacherCondition/${current}/${limit}`,
+            method: 'post',
+            //teacherQuery条件对象,后端使用RequestBody获取数据
+            //data表示把对象转换成json进行传递到接口里面
+            data: teacherQuery
+        })
+    }
+}
